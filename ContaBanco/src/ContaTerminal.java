33743106f0b3113ef35232e85cf951e63ac3b5e5
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2023-04-29 17:54:40 -0300

    E/S dos dados recebidos

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-       
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Informe o número da conta:");
+        int numero = scanner.nextInt();
+        System.out.println("Informe o número a agencia:");
+        String agencia = scanner.next();
+        System.out.println("Informe o saldo da conta:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+ " e seu saldo "+saldo+" já está disponível para saque");
+        scanner.close();
+
+     }
 }
